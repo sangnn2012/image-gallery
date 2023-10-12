f682c404499f401d9a928eb7a84885ffebaf2721
Sang <sangnn2012@gmail.com>
2023-10-12 23:30:23 +0700

    first commit

---
+++ src/user/components/UserList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import UserItem from './UserItem';
+import './UserList.css';
+
+const UserList = (props) => {
+    if (props.item.length === 0) {
+        return (
+            <div className="center">
+                <h2>No users found.</h2>
+            </div>
+        );
+    }
+
+    return <ul>
+        {props.items.map((user) => {
+            return <UserItem
+                key={`user-${user.id}`}
+                id={user.id}
+                image={user.image}
+                name={user.name}
+                placeCount={user.places}
+            />;
+        })}
+    </ul>
+};
+
+export default UserList;
---
+++ src/user/pages/User.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import UserList from '../components/UserList';
+
+const User = () => {
+    return <UserList />
+};
+
+export default User;
