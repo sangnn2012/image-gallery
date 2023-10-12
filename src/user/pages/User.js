4a9ed365dd3309b77eb8ca3f2a2a296f0ec8cea9
Sang <sangnn2012@gmail.com>
2023-10-12 23:34:28 +0700

    feat: add dummy data for User page

--- src/user/pages/User.js
+++ src/user/pages/User.js
@@ -3,7 +3,13 @@ import React from 'react';
 import UserList from '../components/UserList';
 
 const User = () => {
-    return <UserList />
+    const USERS = [{
+        id: 'u1',
+        name: 'Sang Nguyen',
+        image: 'http://www.shadowsphotography.co/wp-content/uploads/2017/12/photography-01-800x400.jpg',
+        places: 3,
+    }];
+    return <UserList users={USERS} />
 };
 
 export default User;
