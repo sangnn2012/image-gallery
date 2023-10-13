18218c75966ad05d0a050fbb003173b881f4debc
Sang <sangnn2012@gmail.com>
2023-10-13 10:54:10 +0700

    feat: add navbar

--- src/user/components/UserList.js
+++ src/user/components/UserList.js
@@ -1,19 +1,21 @@
 import React from 'react';
 
 import UserItem from './UserItem';
-
+import Card from '../../shared/components/UIElement/Card';
 import './UserList.css';
 
 const UserList = (props) => {
     if (props.items.length === 0) {
         return (
             <div className="center">
-                <h2>No users found.</h2>
+                <Card>
+                    <h2>No users found.</h2>
+                </Card>
             </div>
         );
     }
 
-    return <ul class="user-list">
+    return <ul className="user-list">
         {props.items.map((user) => {
             return <UserItem
                 key={`user-${user.id}`}
