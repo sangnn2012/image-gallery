026eada937235f9a3fd2ef20b8d0a3c50ccb1073
Sang <sangnn2012@gmail.com>
2023-10-16 10:33:37 +0700

    feat: add submit handling for new place form

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -54,7 +54,15 @@ const NewPlace = () => {
         dispatch({type: 'INPUT_CHANGE', value, isValid, inputId: id})
     }, []);
 
-    return <form className="place-form">
+    const placeSubmitHandler = (event) => {
+        event.preventDefault();
+        console.log(formState.inputs);
+    }
+
+    return <form
+        className="place-form"
+        onSubmit={placeSubmitHandler}
+    >
         <Input
             id="title"
             element="input"
@@ -70,6 +78,14 @@ const NewPlace = () => {
             label="Description"
             validators={[VALIDATOR_MINLENGTH(5)]}
             errorText="Please enter a valid description (at least 5 characters)."
+            onInput={inputHandler}
+        />
+        <Input
+            id="address"
+            element="input"
+            label="Address"
+            validators={[VALIDATOR_REQUIRE()]}
+            errorText="Please enter a valid address."
             onInput={inputHandler}
         />
         <Button type="submit" disabled={!formState.isValid}>
