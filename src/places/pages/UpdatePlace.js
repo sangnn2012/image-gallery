e6b73e2731b02f1d2480942dac26cbf272c4a370
Sang Nguyen <sangnn2012@gmail.com>
2023-10-18 10:12:04 +0700

    feat: adjusting form hook

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import Input from '../../shared/components/FormElements/Input';
@@ -38,20 +38,41 @@ const DUMMY_PLACES = [
 ];
 
 const UpdatePlace = () => {
+    const [isLoading, setIsLoading] = useState(true);
     const placeId = useParams().placeId;
+
+    const [formState, inputHandler, setFormData] = useForm(
+        {
+            title: {
+                value: '',
+                isValid: false,
+            },
+            description: {
+                value: '',
+                isValid: false,
+            }
+        }, 
+        false,
+    );
 
     const identifiedPlace = DUMMY_PLACES.find((p) => p.id === placeId);
 
-    const [formState, inputHandler] = useForm({
-        title: {
-            value: identifiedPlace.title,
-            isValid: true,
-        },
-        description: {
-            value: identifiedPlace.description,
-            isValid: true,
-        }
-    }, true);
+    useEffect(() => {
+        setFormData(
+            {
+                title: {
+                    value: identifiedPlace.title,
+                    isValid: true,
+                },
+                description: {
+                    value: identifiedPlace.description,
+                    isValid: true,
+                }
+            }, 
+            true,
+        );
+        setIsLoading(false);
+    }, [setFormData, identifiedPlace]);
 
     const placeUpdateSubmitHandler = (event) => {
         event.preventDefault();
@@ -66,35 +87,43 @@ const UpdatePlace = () => {
         )
     }
 
+    if (isLoading) {
+        return (
+            <div className="center">
+                <h2>Loading...</h2>
+            </div>
+        )
+    }
+
     return <form
-        className="place-form"
-        onSubmit={placeUpdateSubmitHandler}
-    >
-        <Input
-            id="title"
-            element="input"
-            type="text"
-            label="Title"
-            validators={[VALIDATOR_REQUIRE()]}
-            errorText="Please enter a valid title"
-            onInput={inputHandler}
-            initialValue={formState.inputs.title.value}
-            initialIsValid={formState.inputs.title.isValid}
-        />
-        <Input
-            id="description"
-            element="textarea"
-            label="Description"
-            validators={[VALIDATOR_MINLENGTH(5)]}
-            errorText="Please enter a valid description (at least 5 characters)."
-            onInput={inputHandler}
-            initialValue={formState.inputs.description.value}
-            initialIsValid={formState.inputs.description.isValid}
-        />
-        <Button type="submit" disabled={!formState.isValid}>
-            UPDATE PLACE
-        </Button>
-    </form>
+            className="place-form"
+            onSubmit={placeUpdateSubmitHandler}
+        >
+            <Input
+                id="title"
+                element="input"
+                type="text"
+                label="Title"
+                validators={[VALIDATOR_REQUIRE()]}
+                errorText="Please enter a valid title"
+                onInput={inputHandler}
+                initialValue={formState.inputs.title.value}
+                initialIsValid={formState.inputs.title.isValid}
+            />
+            <Input
+                id="description"
+                element="textarea"
+                label="Description"
+                validators={[VALIDATOR_MINLENGTH(5)]}
+                errorText="Please enter a valid description (at least 5 characters)."
+                onInput={inputHandler}
+                initialValue={formState.inputs.description.value}
+                initialIsValid={formState.inputs.description.isValid}
+            />
+            <Button type="submit" disabled={!formState.isValid}>
+                UPDATE PLACE
+            </Button>
+        </form>
 };
 
 export default UpdatePlace;
